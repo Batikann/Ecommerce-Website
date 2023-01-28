2500b3f900cf03c3a4adaa66264d76c61521a1ed
batikan <emiruar123@gmail.com>
2023-01-28 23:56:08 +0300

    lightbox added

--- shop.js
+++ shop.js
@@ -1,45 +1,58 @@
 let mainImg=document.getElementById("mainImg");
 let smallImg=document.getElementsByClassName("small-img");
+let smallImgCol=document.getElementsByClassName('small-img-col')
+
 
 
 smallImg[0].onclick=function() {
     mainImg.src=smallImg[0].src;
+
     for (let i = 0; i < smallImg.length; i++) {
-        if (smallImg[i].classList.contains("active-2")) {
-            smallImg[i].classList.remove('active-2');
+        if (smallImgCol[i].classList.contains("active-2") && smallImg[i].classList.contains("active-3")) {
+            smallImgCol[i].classList.remove('active-2');
+            smallImg[i].classList.remove("active-3");
         }
         
     }
-    smallImg[0].classList.add('active-2');
+    smallImgCol[0].classList.add('active-2');
+    smallImg[0].classList.add("active-3");
 }
 smallImg[1].onclick=function() {
     mainImg.src=smallImg[1].src;
+
     for (let i = 0; i < smallImg.length; i++) {
-        if (smallImg[i].classList.contains("active-2")) {
-            smallImg[i].classList.remove('active-2');
+        if (smallImgCol[i].classList.contains("active-2") && smallImg[i].classList.contains("active-3")) {
+            smallImgCol[i].classList.remove('active-2');
+            smallImg[i].classList.remove("active-3");
         }
         
     }
-    smallImg[1].classList.add('active-2');
+    smallImgCol[1].classList.add('active-2');
+    smallImg[1].classList.add("active-3");
 }
 smallImg[2].onclick=function() {
     mainImg.src=smallImg[2].src;
 
     for (let i = 0; i < smallImg.length; i++) {
-        if (smallImg[i].classList.contains("active-2")) {
-            smallImg[i].classList.remove('active-2');
+        if (smallImgCol[i].classList.contains("active-2") && smallImg[i].classList.contains("active-3")) {
+            smallImgCol[i].classList.remove('active-2');
+            smallImg[i].classList.remove("active-3");
         }
         
     }
-    smallImg[2].classList.add('active-2');
+    smallImgCol[2].classList.add('active-2');
+    smallImg[2].classList.add("active-3");
 }
 smallImg[3].onclick=function() {
     mainImg.src=smallImg[3].src;
+
     for (let i = 0; i < smallImg.length; i++) {
-        if (smallImg[i].classList.contains("active-2")) {
-            smallImg[i].classList.remove('active-2');
+        if (smallImgCol[i].classList.contains("active-2") && smallImg[i].classList.contains("active-3")) {
+            smallImgCol[i].classList.remove('active-2');
+            smallImg[i].classList.remove("active-3");
         }
         
     }
-    smallImg[3].classList.add('active-2');
+    smallImgCol[3].classList.add('active-2');
+    smallImg[3].classList.add("active-3");
 }
